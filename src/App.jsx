4fee7b526bf5d54b14169520436bbe7feaf38148
Pierre Silva de Sá <pierresa2005@gmail.com>
2023-08-14 15:12:47 -0300

    aula_14_8

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,21 @@ import { useState } from "react"
 import "./App.css"
 
 export function App() {
+  const [usuario, setUsuario] = useState("")
+  const [senha, setSenha] = useState("")
   
+  function hendleUsuario(evento) {
+    setUsuario(evento.target.value)
+  }
+  function hendleSenha(evento) {
+    setSenha(evento.target.value)
+  }
+  console.log(usuario)
+  console.log(senha)
+
+
+
+
   return (
     <>
       <header>
@@ -13,11 +27,11 @@ export function App() {
         <form action="">
           <div>
             <label htmlFor="usuario">Usuário</label>
-            <input type="text" id="usuario" placeholder="Digite seu usuário" />
+            <input type="text" id="usuario" placeholder="Digite seu usuário" onChange={hendleUsuario} />
           </div>
           <div>
             <label htmlFor="senha">Senha</label>
-            <input type="password" id="senha" placeholder="Digite seu senha" />
+            <input type="password" id="senha" placeholder="Digite seu senha" onChange={hendleSenha} />
           </div>
           
           <button>Entrar</button>
